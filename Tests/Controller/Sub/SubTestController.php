4a88a0c9767f67a3f4217c206445591e41425b0e
Sławomir Kaleta <slaszka@gmail.com>
2020-12-13 13:05:40 +0100

    Resolved #182, Move Test into separate folders

---
+++ Tests/Controller/Sub/SubTestController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Dframe\Loader\Tests\Controller\Sub;
+
+use Dframe\Controller;
+
+class SubTestController extends Controller
+{
+    /**
+     * @return bool
+     */
+    public function test()
+    {
+        return true;
+    }
+}
